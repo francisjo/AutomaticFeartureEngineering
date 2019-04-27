31d2fc20c231b2f7ef2a04e70c155607953b50a4
francisjo <joseph4it@gmail.com>
2019-04-27 16:48:12 +0200

    Add multiple transformer for categorical(OneHotEncoding and Binaryencoding)

--- baseline_pipeline.py
+++ baseline_pipeline.py
@@ -9,6 +9,7 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 import pandas.api.types as ptypes
+import category_encoders as ce
 
 df = pd.read_csv('https://raw.githubusercontent.com/nphardly/titanic/master/data/inputs/train.csv')
 #df = pd.read_csv('https://raw.githubusercontent.com/francisjo/AutomaticFeartureEngineering/master/Datasets/heart.csv')
@@ -20,12 +21,11 @@ threshold = 0.20
 targetLabel = 'Survived'
 ### Replace Null values to NaN
 df.fillna(np.NaN,inplace=True)
-
 ##convert Date col to Datetime and split it to Year,Month,Day Columns###
 #df['Date'] = pd.to_datetime(df['Date'])
 def splitDatecol():
     
-    for col in df.columns:
+    for col in df.columns:  
         if(ptypes.is_datetime64_ns_dtype(df[col])):
             df[col+'_Day'] = pd.DatetimeIndex(df[col]).day
             df[col+'_Month'] = pd.DatetimeIndex(df[col]).month
@@ -48,36 +48,51 @@ def classifyColumnsWithThreshold(threshold=0.2):
 splitDatecol()             
 classifyColumnsWithThreshold(threshold)
 ################################################
-
-# We create the preprocessing pipelines for both numeric and categorical data.
-# 1- For Numeric data
-numeric_transformer = Pipeline(steps=[
-    ('imputer', SimpleImputer(missing_values=np.NaN, strategy='mean',copy=False)),
-    ('scaler', StandardScaler())])
-# 2 -For categorical data
-categorical_transformer = Pipeline(steps=[
-    ('imputer', SimpleImputer(strategy="most_frequent",copy=False))
-    ,('onehot', OneHotEncoder(handle_unknown='ignore'))
-    ])
-# 3- Combine Transformers in one Preprocessor  
-preprocessor = ColumnTransformer(
-    transformers=[
+def categorical_transformermethod(categoricalmethod1):
+    
+    if (categoricalmethod1 =='BinaryEncoder'):
+        categorical_transformer = Pipeline(steps=[('imputer', SimpleImputer(strategy="most_frequent",copy=False))
+        ,('BinaryEncoder', ce.BinaryEncoder(handle_unknown='impute'))
+        ])
+    elif (categoricalmethod1 =='OneHotEncoder'):
+         categorical_transformer = Pipeline(steps=[
+        ('imputer', SimpleImputer(strategy="most_frequent",copy=False))
+        ,('onehot', OneHotEncoder(handle_unknown='ignore'))
+        ])
+    return categorical_transformer
+#################################################
+    
+def run_modul(categoricalmethod='OneHotEncoder'):
+ # We create the preprocessing pipelines for both numeric and categorical data.
+        # 1- For Numeric data
+        numeric_transformer = Pipeline(steps=[
+        ('imputer', SimpleImputer(missing_values=np.NaN, strategy='mean',copy=False)),
+        ('scaler', StandardScaler())])
+        # 2 -For categorical data
+        categorical_transformer=categorical_transformermethod(categoricalmethod)
+        # 3- Combine Transformers in one Preprocessor 
+        preprocessor = ColumnTransformer(
+        transformers=[
         ('num', numeric_transformer, numericList),
         ('cat', categorical_transformer, categoricalList)])
-
-# Append classifier to preprocessing pipeline.
-# Now we have a full prediction pipeline.
-clf = Pipeline(steps=[('preprocessor', preprocessor),
-                      ('classifier', LogisticRegression(solver='lbfgs'))])
-
-##### Features
-X = df.drop(targetLabel, axis=1)
-##### Target Label
-y = df[targetLabel]
-
-#### Split Data to Train and Test Data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
-
-# Fit Modul
-clf.fit(X_train, y_train)
-print("model score: %.3f" % clf.score(X_test, y_test))
+        # Append classifier to preprocessing pipeline.
+        # Now we have a full prediction pipeline.
+        clf = Pipeline(steps=[('preprocessor', preprocessor),
+          ('classifier', LogisticRegression(solver='lbfgs'))])
+        ##### Features
+        X = df.drop(targetLabel, axis=1)
+        ##### Target Label
+        y = df[targetLabel]
+        #### Split Data to Train and Test Data
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+        
+        # Fit Modul
+        clf.fit(X_train, y_train)
+        return clf.score(X_test, y_test)
+#########################################################################
+        
+classicalencoder = ['OneHotEncoder','BinaryEncoder']
+for name in classicalencoder:
+    print(name)
+    result = run_modul(name)
+    print("model score with" + name + ":%.3f" %  result)
