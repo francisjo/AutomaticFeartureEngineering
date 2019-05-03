1f28e63a7244d7993dfbf85c1e37e1f55fe8bfcc
AhmedSalahBasha <a.salah.b@gmail.com>
2019-05-03 02:05:44 +0200

    structure improvements

--- baseline_pipeline.py
+++ baseline_pipeline.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import pandas as pd
 import numpy as np
 
@@ -8,8 +7,10 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-import pandas.api.types as ptypes
-import category_encoders as ce
+
+from columns_classification import ClassifyColumns as cc
+from data_cleaning import CleaningData as cd
+from data_preprocessing import PreprocessingData as predata
 
 titanic = 'https://raw.githubusercontent.com/nphardly/titanic/master/data/inputs/train.csv'
 heart = 'https://raw.githubusercontent.com/francisjo/AutomaticFeartureEngineering/master/Datasets/heart.csv'
@@ -17,120 +18,67 @@ car = 'https://raw.githubusercontent.com/francisjo/AutomaticFeartureEngineering/
 wikipedia = 'https://raw.githubusercontent.com/francisjo/AutomaticFeartureEngineering/master/Datasets/smallwikipedia.csv'
 
 df = pd.read_csv(titanic)
-#df = pd.read_csv(heart)
-#df = pd.read_csv(car)
-#df = pd.read_csv(wikipedia, delimiter=';')  #for datetime column
+# df = pd.read_csv(heart)
+# df = pd.read_csv(car)
+# df = pd.read_csv(wikipedia, delimiter=';')  #for datetime column
 
-## global variables
+# global variables
 threshold = 0.20
 targetLabel = 'Survived'
 
-##### fill null values with respect to the column data-type ######
-def fill_null_data(col, col_type):
-    if (df[col].isnull().sum() > 0 and col_type == 'string'):
-        df[col].fillna('missing', inplace=True)
-    elif (df[col].isnull().sum() > 0 and col_type == 'numeric'):
-        df[col].fillna(df[col].mean(), inplace=True)
-            
-#####convert Date col to Datetime and split it to Year,Month,Day Columns###
-def split_datetime_col():
-    #df['Date'] = pd.to_datetime(df['Date'])
-    for col in df.columns:  
-        if(ptypes.is_datetime64_ns_dtype(df[col])):
-            df[col+'_Day'] = pd.DatetimeIndex(df[col]).day
-            df[col+'_Month'] = pd.DatetimeIndex(df[col]).month
-            df[col+'_Year'] = pd.DatetimeIndex(df[col]).year
-         
-##### Classify Columns Wih Threshold####################
-def classify_columns_statistically(threshold=0.2):
-    numeric_list = []
-    categoric_list = []
-    for col in df.columns:
-        if (col == targetLabel):
-            continue
-        elif (ptypes.is_string_dtype(df[col])):
-            fill_null_data(col, 'string')
-            categoric_list.append(col)
-        elif (ptypes.is_numeric_dtype(df[col])):
-            fill_null_data(col, 'numeric')
-            col_count = len(df[col].unique())
-            percent = col_count / df[col].count()
-            if (percent < threshold):
-                categoric_list.append(col)
-            else:
-                numeric_list.append(col)
-    return numeric_list, categoric_list
 
-##### Transform the categorical data by different encoding methods ###########
-def categorical_transformer_method(categorical_method):
-    simple_imputer = SimpleImputer(strategy="most_frequent",copy=False)
-    if (categorical_method =='BinaryEncoder'):
-        categorical_transformer = Pipeline(
-                steps=[
-                        ('imputer', simple_imputer),
-                        ('BinaryEncoder', ce.BinaryEncoder())
-                ])
-    elif (categorical_method =='OneHotEncoder'):
-        categorical_transformer = Pipeline(
-                steps=[
-                        ('imputer', simple_imputer),
-                        ('onehot', ce.OneHotEncoder())
-                ])
-    elif (categorical_method =='HashingEncoder'):
-        categorical_transformer = Pipeline(
-                steps=[
-                        ('imputer', simple_imputer),
-                        ('onehot', ce.HashingEncoder())
-                ])
-    return categorical_transformer
-
-##### the main function to run the preprocessing phase and to fit the model to training data #######
+# the main function to run the preprocessing phase and to fit the model to training data #
 def run_model(categorical_method):
     
     # split datatime column if exists
-    split_datetime_col()
+    cd.split_datetime_col(df)
     
     # classify columns to categorical and numerical
-    numerical_features, categorical_features = classify_columns_statistically(threshold)
+    numerical_features, categorical_features = cc.columns_distribution_classification(df, targetLabel)
     
     # impute and scaling numerical features
     numeric_transformer = Pipeline(steps=[
-    ('imputer', SimpleImputer(missing_values=np.NaN, strategy='mean',copy=False)),
-    ('scaler', StandardScaler())])
+        ('imputer', SimpleImputer(missing_values=np.NaN, strategy='mean', copy=False)),
+        ('scaler', StandardScaler())])
     
     # try many different categorical data encoders
-    categorical_transformer = categorical_transformer_method(categorical_method)
+    categorical_transformer = predata.categorical_transformer_method(categorical_method)
     
     # combine transformers in one Preprocessor 
     preprocessor = ColumnTransformer(
-            transformers=[
-                    ('num', numeric_transformer, numerical_features),
-                    ('cat', categorical_transformer, categorical_features)
-                    ]
-            )
+        transformers=
+        [
+            ('num', numeric_transformer, numerical_features),
+            ('cat', categorical_transformer, categorical_features)
+        ]
+    )
     
     # append classifier to preprocessing pipeline.
-    classifier = Pipeline(steps=[
+    classifier = Pipeline(
+        steps=
+        [
             ('preprocessor', preprocessor),
             ('classifier', LogisticRegression(solver='lbfgs', max_iter=1000))
-            ])
+        ]
+    )
     
-    ##### Features
+    # Features
     X = df.drop(targetLabel, axis=1)
     
-    ##### Target Label
+    # Target Label
     y = df[targetLabel]
     
-    #### Split Data to Train and Test Data
+    # Split Data to Train and Test Data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
     
     # Fit Model
     classifier.fit(X_train, y_train)
-    return classifier.score(X_test, y_test)
-        
+    score = classifier.score(X_test, y_test)
+    return score
 
-classical_encoders = ['OneHotEncoder','BinaryEncoder', 'HashingEncoder']
+
+classical_encoders = ['OneHotEncoder', 'BinaryEncoder', 'HashingEncoder']
 for encoder in classical_encoders:
     result = run_model(encoder)
-    print("Model Score with: " + encoder + " : %.3f" %  result)
+    print("Model Score with: " + encoder + " : %.3f" % result)
     print('--------------')
