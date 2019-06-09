0a0144c07f9b0ab4095e8ab81bd1fa275159d176
francisjo <joseph4it@gmail.com>
2019-06-09 14:55:34 +0200

    add grid_search

--- baseline_pipeline.py
+++ baseline_pipeline.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn import tree
 
@@ -21,7 +21,9 @@ def run_model_tree(df):
     # Split Data to Train and Test Data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0, random_state=42)
     # Fit Model
-    clf = tree.DecisionTreeClassifier(criterion="gini", random_state=3)
+    clf = tree.DecisionTreeClassifier(random_state=3)
+    grid_search_method(clf, X_train, y_train)
+    '''
     clf.fit(X_train, y_train)
 
     # ==== Test Model on new Data [bridges.csv] ====
@@ -52,31 +54,61 @@ def run_model_tree(df):
     print("Decision Tree Score: ", score)
 
 
-    # Applying K-Fold Cross Validation
-    print("------K-Fold Cross Validation-------")
-    accuracies = cross_val_score(estimator=clf, X=X_test, y=y_test, cv=5)
-    print("Cross-Validation Accuracies:  ", accuracies)
-    print("Cross-Validation Mean Accuracy =  ", accuracies.mean())
 
-    predictdf = pd.DataFrame(y_predict)
-    predictdf.columns = ['new_clf']
+
+
+    #predictdf = pd.DataFrame(y_predict)
+    #predictdf.columns = ['new_clf']
 
     print("--- Ground-truth and Prediction Results ---")
     final_clf_df = pd.concat([y_test.reset_index(), predictdf], axis=1)
     print(final_clf_df)
     return final_clf_df
+    '''
 
 
 def main_func():
     w2v.init()
-    df_dict = ld.load_data_online()
+    df_dict = ld.load_data_local()
     # df_dict = ld.load_data_local()
     summarized_dfs = col_classify.get_summarized_df(df_dict)
     summarized_dfs = summarized_dfs.set_index("index")
-    final_clf_df = run_model_tree(summarized_dfs)
-    adult = 'https://raw.githubusercontent.com/francisjo/AutomaticFeartureEngineering/master/Datasets/adult.csv'
-    adult_df = pd.read_csv(adult)
-    enc_meth.run_model_representation(adult_df, final_clf_df)
+    # final_clf_df = run_model_tree(summarized_dfs)
+    run_model_tree(summarized_dfs)
+    # adult = 'https://raw.githubusercontent.com/francisjo/AutomaticFeartureEngineering/master/Datasets/adult.csv'
+    # adult_df = pd.read_csv(adult)
+    # enc_meth.run_model_representation(adult_df, final_clf_df)
+
+
+def grid_search_method(classifier, X_train, y_train):
+    grid_param = {
+        'n_estimators': [100, 300, 500, 800, 1000],
+        'criterion': ['gini', 'entropy'],
+        'splitter': ['best', 'random'],
+        'bootstrap': [True, False],
+        'max_depth ': [3, 4, 5, 6, 7, 8, 9, 10],
+        'min_samples_split': [2, 3, 4, 5, 6],
+        'min_samples_leaf': [1, 2, 3, 4, 5, 6],
+        'max_features': [None, "auto", "sqrt", "log2"]
+    }
+    gd_sr = GridSearchCV(estimator=classifier,
+                         param_grid=grid_param,
+                         scoring='accuracy',
+                         cv=5,
+                         n_jobs=-1)
+    gd_sr.fit(X_train, y_train)
+    best_parameters = gd_sr.best_params_
+    best_result = gd_sr.best_score_
+    print("best_parameters: ", best_parameters)
+    print("Best score: ", best_result)
+
+
+def cross_validation_method(clf, X_test, y_test):
+    # Applying K-Fold Cross Validation
+    print("------K-Fold Cross Validation-------")
+    accuracies = cross_val_score(estimator=clf, X=X_test, y=y_test, cv=5)
+    print("Cross-Validation Accuracies:  ", accuracies)
+    print("Cross-Validation Mean Accuracy =  ", accuracies.mean())
 
 
 main_func()
--- src/word2vec_load.py
+++ src/word2vec_load.py
@@ -7,6 +7,9 @@ def init():
     global w2v_model
     global d2v_model
     #model = KeyedVectors.load_word2vec_format('/home/basha/Desktop/GoogleNews-vectors-negative300.bin', binary=True)
-    w2v_model = KeyedVectors.load('/home/basha/PycharmProjects/DSA_Project/AutomaticFeartureEngineering/Datasets/Word2VecModel/word2vec.model')
-    d2v_model = Doc2Vec.load('/home/basha/PycharmProjects/DSA_Project/AutomaticFeartureEngineering/Datasets/Doc2VecModel/enwiki_dbow/doc2vec.bin')
 
+    w2v_model = KeyedVectors.load('C:\\Users\\Joseph Francis\\AutomaticFeartureEngineering\\Datasets\\Word2VecModel\\word2vec.model')
+    d2v_model = Doc2Vec.load('C:\\Users\\Joseph Francis\\AutomaticFeartureEngineering\\Datasets\\Doc2VecModel\\doc2vec.model')
+#model = KeyedVectors.load_word2vec_format('/home/basha/Desktop/GoogleNews-vectors-negative300.bin', binary=True)
+  #  w2v_model = KeyedVectors.load('/home/basha/PycharmProjects/DSA_Project/AutomaticFeartureEngineering/Datasets/Word2VecModel/word2vec.model')
+   #    d2v_model = Doc2Vec.load('/home/basha/PycharmProjects/DSA_Project/AutomaticFeartureEngineering/Datasets/Doc2VecModel/enwiki_dbow/doc2vec.bin')
